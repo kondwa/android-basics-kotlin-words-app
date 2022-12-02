82e69c11c9debec6e376736ecdfca0e566b0f01f
Kondwani Hara <kondwa@gmail.com>
2022-12-02 14:56:06 +0200

    changes made

--- app/src/main/java/com/example/wordsapp/LetterListFragment.kt
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -24,16 +24,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.wordsapp.data.SettingsDataStore
 import com.example.wordsapp.databinding.FragmentLetterListBinding
+import kotlinx.coroutines.launch
 
 /**
  * Entry fragment for the app. Displays a [RecyclerView] of letters.
  */
 class LetterListFragment : Fragment() {
     private var _binding: FragmentLetterListBinding? = null
+    private lateinit var settingsDataStore:SettingsDataStore
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -63,7 +68,12 @@ class LetterListFragment : Fragment() {
         recyclerView = binding.recyclerView
         // Sets the LayoutManager of the recyclerview
         // On the first run of the app, it will be LinearLayoutManager
-        chooseLayout()
+        settingsDataStore = SettingsDataStore(requireContext())
+        settingsDataStore.preferenceFlow.asLiveData().observe(viewLifecycleOwner){
+            isLinearLayoutManager = it
+            chooseLayout()
+            activity?.invalidateOptionsMenu()
+        }
     }
 
     /**
@@ -115,6 +125,9 @@ class LetterListFragment : Fragment() {
                 // Sets isLinearLayoutManager (a Boolean) to the opposite value
                 isLinearLayoutManager = !isLinearLayoutManager
                 // Sets layout and icon
+                lifecycleScope.launch {
+                    settingsDataStore.saveLayoutToPreferencesDataStore(isLinearLayoutManager,requireContext())
+                }
                 chooseLayout()
                 setIcon(item)
 
---
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -0,0 +1,37 @@
+package com.example.wordsapp.data
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+class SettingsDataStore(context: Context) {
+    private val LAYOUT_PREFERENCES_NAME = "layout_preferences"
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = LAYOUT_PREFERENCES_NAME)
+    val preferenceFlow: Flow<Boolean> = context.dataStore.data
+        .catch {
+            if (it is IOException){
+                it.printStackTrace()
+                emit(emptyPreferences())
+            }else{
+                throw it
+            }
+        }
+        .map { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] ?: true
+        }
+    suspend fun saveLayoutToPreferencesDataStore(isLinearLayoutManager: Boolean,context: Context){
+        context.dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
+}
